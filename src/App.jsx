edb4634ede78ad61cbaef5de2728ffe7688af275
guillermo gordon <guillermo_gordon18@hotmail.com>
2023-04-19 08:58:04 -0500

    ERROR

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import Sidebar from "./component/shared/Sidebar";
+import {
+  RiMenu3Fill,
+  RiUser3Line,
+  RiAddLine,
+  RiPieChartLine,
+} from "react-icons/ri";
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOrder, setShowOder] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="bg-[#262837] w-full min-h-screen">
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <Sidebar showMenu={showMenu} />
+      {/* Menu Movil */}
+      <nav className="fixed bottom-0 left-0 w-full bg-[#1F1D2B] lg:hidden text-3xl text-gray-300 py-4 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
+        <button>
+          <RiUser3Line />
+        </button>
+        <button>
+          <RiAddLine />
+        </button>
+        <button>
+          <RiPieChartLine />
+        </button>
+        <button onClick={toggleMenu} className="p-2 text-white">
+          <RiMenu3Fill />
+        </button>
+        <main className="pl-26">
+          <div>Hola </div>
+          <div>Hola 2</div>
+        </main>
+      </nav>
     </div>
   );
 }
--- src/component/shared/Sidebar.jsx
+++ src/component/shared/Sidebar.jsx
@@ -9,7 +9,8 @@ import {
   RiLogoutCircleRLine,
 } from "react-icons/ri";
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+  const { showMenu } = props;
   return (
     <div className="bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl">
       <div>
