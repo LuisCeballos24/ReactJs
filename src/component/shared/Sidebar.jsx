9d916b3e16767844acd09d0a84cd57880081d469
guillermo gordon <guillermo_gordon18@hotmail.com>
2023-04-19 08:16:44 -0500

    Revert "Side Complete"
    This reverts commit c53869b42f56234f2d8d47dc77e0ac62aafe41b0.

--- src/component/shared/Sidebar.jsx
+++ src/component/shared/Sidebar.jsx
@@ -9,14 +9,9 @@ import {
   RiLogoutCircleRLine,
 } from "react-icons/ri";
 
-const Sidebar = (props) => {
-  const { showMenu } = props;
+const Sidebar = () => {
   return (
-    <div
-      className={`bg-[#1F1D2B] fixed lg:left-0 top-0 w-28 h-full flex flex-col justify-between py-6 transition-all rounded-tr-xl rounded-br-xl z-50 -left-full ${
-        showMenu ? "left-0" : "-left-full "
-      } `}
-    >
+    <div className="bg-[#1F1D2B] fixed left-0 top-0 w-28 h-full flex flex-col justify-between py-6 rounded-tr-xl rounded-br-xl">
       <div>
         <ul className="pl-4">
           <li>
