127398bbb992670affb73a6dfdabd29def1fee7a
guillermo gordon <guillermo_gordon18@hotmail.com>
2023-05-08 11:03:38 -0500

    Config 5-27-2sd

--- src/components/shared/Car.jsx
+++ src/components/shared/Car.jsx
@@ -1,12 +1,93 @@
 import React from "react";
 import { RiCloseLine, RiDeleteBin6Line } from "react-icons/ri";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { db, auth } from "../../utils/firebase.js";
+import { orderBy } from "firebase/firestore";
+/* import { Card } from "./Card"; */
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-const Card = (props) => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      setEmail("guillermo18zgordon@gmail.com");
+      setPassword("123456");
+      setError(null);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Iniciar sesión</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Iniciar sesión</button>
+      </form>
+    </div>
+  );
+}
+
+function OrderItem(props) {
+  return (
+    <div className="order-item">
+      <p>{props.name}</p>
+      <p>Precio: ${props.price}</p>
+      <p>Cantidad: {props.quantity}</p>
+      <p>Fecha: {props.createdAt.toDate().toLocaleString()}</p>
+    </div>
+  );
+}
+
+const Card_car = (props) => {
+  /*  let listaPersonas = new Personas(personas); */
   const { showOrder, setShowOrder } = props;
+  const resultado2 = Login;
+
+  const currentUser = auth.currentUser;
+
+  const [orders, loading, error] = useCollectionData(
+    db
+      .collection("order")
+      .where("buyerId", "==", "6J2J5ElYbFURXDW5cYroZ10iE8m1" || "")
+  );
+
+  if (loading) {
+    return <p>Cargando órdenes...</p>;
+  }
+
+  if (error) {
+    return <p>Error al cargar órdenes: {error.message}</p>;
+  }
+
   return (
     <div
-      className={`lg:col-span-2 fixed top-0 bg-[#1F1D2B] w-full lg:w-96 lg:right-0 h-full transition-all z-50 ${showOrder ? "right-0" : "-right-full"
-        }`}
+      className={`lg:col-span-2 fixed top-0 bg-[#1F1D2B] w-full lg:w-96 lg:right-0 h-full transition-all z-50 ${
+        showOrder ? "right-0" : "-right-full"
+      }`}
     >
       {/* Orders */}
       <div className="relative p-8 pt-16 h-full text-gray-300 lg:pt-8">
@@ -34,82 +115,56 @@ const Card = (props) => {
             <h5>Qty</h5>
             <h5>Price</h5>
           </div>
+          //
           {/* Products */}
           <div className="overflow-scroll h-[400px] md:h-[700px] lg:h-[540px]">
             {/* Product */}
-            <div className="bg-[#262837] p-4 rounded-xl mb-4">
-              <div className="grid grid-cols-6 mb-4">
-                {/* Product description */}
-                <div className="flex col-span-4 gap-3 items-center">
-                  <img src="comida.png" className="object-cover w-10 h-10" />
+            {orders.map((order) => (
+              <OrderItem
+                key={order.productId}
+                name={order.name}
+                price={order.price}
+                quantity={order.quantity}
+                createdAt={order.createdAt}
+              />
+            ))}
+            {orders.map((order) => (
+              <div className="bg-[#262837] p-4 rounded-xl mb-4">
+                <div className="grid grid-cols-6 mb-4">
+                  {/* Product description */}
+                  <div className="flex col-span-4 gap-3 items-center">
+                    <img src="comida.png" className="object-cover w-10 h-10" />
+                    <div>
+                      <h5 className="text-sm"> {order.name}</h5>
+                      <p className="text-xs text-gray-500"></p>
+                    </div>
+                  </div>
+                  {/* Qty */}
                   <div>
-                    <h5 className="text-sm">Spaicy seaso...</h5>
-                    <p className="text-xs text-gray-500">$2.29</p>
+                    <span>2</span>
+                  </div>
+                  {/* Price */}
+                  <div>
+                    <span>$4.58</span>
                   </div>
                 </div>
-                {/* Qty */}
-                <div>
-                  <span>2</span>
-                </div>
-                {/* Price */}
-                <div>
-                  <span>$4.58</span>
-                </div>
-              </div>
-              {/* Note */}
-              <div className="grid grid-cols-6 items-center">
-                <form className="col-span-5">
-                  <input
-                    type="text"
-                    className="bg-[#1F1D2B] py-2 px-4 rounded-lg outline-none"
-                    placeholder="Order note..."
-                  />
-                </form>
-                <div>
-                  <button className="p-2 rounded-lg border border-red-500">
-                    <RiDeleteBin6Line className="text-red-500" />
-                  </button>
+                {/* Note */}
+                <div className="grid grid-cols-6 items-center">
+                  <form className="col-span-5">
+                    <input
+                      type="text"
+                      className="bg-[#1F1D2B] py-2 px-4 rounded-lg outline-none"
+                      placeholder="Order note..."
+                    />
+                  </form>
+                  <div>
+                    <button className="p-2 rounded-lg border border-red-500">
+                      <RiDeleteBin6Line className="text-red-500" />
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
-            {/* Product */}
-            {/* <div className="bg-[#262837] p-4 rounded-xl mb-4"> */}
-            {/*   <div className="grid grid-cols-6 mb-4"> */}
-            {/*     {/* Product description */}
-            {/*     <div className="flex col-span-4 gap-3 items-center"> */}
-            {/*       <img src="comida.png" className="object-cover w-10 h-10" /> */}
-            {/*       <div> */}
-            {/*         <h5 className="text-sm">Spaicy seaso...</h5> */}
-            {/*         <p className="text-xs text-gray-500">$2.29</p> */}
-            {/*       </div> */}
-            {/*     </div> */}
-            {/*     {/* Qty */}
-            {/*     <div> */}
-            {/*       <span>2</span> */}
-            {/*     </div> */}
-            {/*     {/* Price */}
-            {/*     <div> */}
-            {/*       <span>$4.58</span> */}
-            {/*     </div> */}
-            {/*   </div> */}
-            {/*   {/* Note */}
-            {/*   <div className="grid grid-cols-6 items-center"> */}
-            {/*     <form className="col-span-5"> */}
-            {/*       <input */}
-            {/*         type="text" */}
-            {/*         className="bg-[#1F1D2B] py-2 px-4 rounded-lg outline-none" */}
-            {/*         placeholder="Order note..." */}
-            {/*       /> */}
-            {/*     </form> */}
-            {/*     <div> */}
-            {/*       <button className="p-2 rounded-lg border border-red-500"> */}
-            {/*         <RiDeleteBin6Line className="text-red-500" /> */}
-            {/*       </button> */}
-            {/*     </div> */}
-            {/*   </div> */}
-            {/* </div> */}
-            {/**/}
-            {/* Product */}
+            ))}
           </div>
         </div>
         {/* Submit payment */}
@@ -120,7 +175,7 @@ const Card = (props) => {
           </div>
           <div className="flex justify-between items-center mb-6">
             <span className="text-gray-400">Subtotal</span>
-            <span>$201.03</span>
+            <span id="precio_total"></span>
           </div>
           <div>
             <button className="bg-[#ec7c6a] w-full py-2 px-4 rounded-lg">
@@ -133,4 +188,4 @@ const Card = (props) => {
   );
 };
 
-export default Card;
+export default Card_car;
--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,14 +1,35 @@
 import React from "react";
+import { useState } from "react";
+import Card_car from "./Car";
+import { RiCloseLine } from "react-icons/ri";
+import { BsPlusSquareFill } from "react-icons/bs";
+let personas = [];
+personas.push({ nombre: "Juan", edad: 25 });
+personas.push({ nombre: "María", edad: 30 });
+personas.push({ nombre: "Pedro", edad: 20 });
 
-const handleClick = () => {
+const handleClick = (name) => {
   console.log("------- Agregado -----");
   const id = event.target.id;
-  console.log("Se hizo clic en el elemento con el id:", id);
+  console.log(id);
+  return (
+    <div className="border-gray-50">
+      sds
+      <Card_car />
+    </div>
+  );
 };
 
-const Card = (props) => {
-  const { id, img, description, price, inventory } = props;
+function mostrarPersonas() {
+  for (let i = 0; i < this.personas.length; i++) {
+    console.log(`${this.personas[i].nombre} - ${this.personas[i].edad} años`);
+  }
+}
 
+const Card = (props) => {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showOrder, setShowOrder] = useState(false);
+  const { name, img, description, price, inventory } = props;
   return (
     <div
       id="Product"
@@ -18,15 +39,15 @@ const Card = (props) => {
         src={img}
         className="object-cover -mt-20 w-40 h-40 rounded-full shadow-2xl"
       />
-      <p className="text-xl">{description}</p>
+      <p className="text-xl">{name}</p>
       <span className="text-gray-400">${price}</span>
       <p className="text-gray-600">{inventory} Bowls available</p>
       <button //className="flex items-center gap-4 text-gray-300 bg-[#1F1D2B] py-2 px-4 rounded-lg"
-        className="bg-[#ec7c6a]  py-2 px-4 rounded-lg"
+        className="p-2 rounded-lg border border-green-500"
         onClick={handleClick}
-        id={id}
+        id={name}
       >
-        Agregar
+        <BsPlusSquareFill className="text-green-500" />
       </button>
     </div>
   );
