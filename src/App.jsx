a93200deeff2f4d620b54c3cbd7e654a4d7703df
guillermo gordon <guillermo_gordon18@hotmail.com>
2023-05-14 17:30:31 -0500

    Swith modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
-// import React from "react";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import HOME_PAGE from "./pages/shared/Home";
 import Home_page_user from "./pages/shared/Home_page_user";
 
 function App() {
-  {
-    /* <Switch> */
-  }
-  {
-    /*        <Route path="/" component={Main} />it */
-  }
-  {
-    /*      </Switch> */
-  }
-  {
-    /**/
-  }
-  return <HOME_PAGE />;
+  return (
+    <Switch>
+      <Route path="/" component={HOME_PAGE} />
+      <Route path="/user" component={Home_page_user} />
+    </Switch>
+  );
 }
 
 export default App;
